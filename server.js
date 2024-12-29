4e0d0d52932ccdd9fb69e9b352bd140af97fb815
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-12-28 23:20:34 -0300

    fix: corrige as importacoes e rotas

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
-import { connectDB } from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
+import { connectDB } from './app/config/db.js';
+import authRoutes from './app/routes/authRoutes.js';
+import userRoutes from './app/routes/userRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/auth/user', authRoutes);
+app.use('/api/auth/user', authRoutes);
+app.use('/api', userRoutes);
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.status(200).json({ msg: "bem vindo" })
 });
 
